a462dc082c0c5cbdf077dc5a31899e72d13594ee
iohhng <43526283+iohhng@users.noreply.github.com>
2024-09-10 19:47:49 -0400

    Update project_workbench.lua

--- lua/workbench/project_workbench.lua
+++ lua/workbench/project_workbench.lua
@@ -1,12 +1,11 @@
 local project_workbench = {}
-local utils = require('workbench.utils')
 
 -- create a global bufnr
 project_workbench_bufnr = vim.api.nvim_create_buf(false, true)
 project_workbench_initialized = false
 
 function project_workbench.filepath()
-  return vim.fn.expand('~/workspace/todo.md')
+  return "~/workspace/todo.md"
 end
 
 function project_workbench.initialize()
@@ -18,17 +17,20 @@ function project_workbench.initialize()
 
   project_workbench_initialized = true
 
-  local win_id = vim.api.nvim_open_win(project_workbench_bufnr, true, utils.window_config(width, height))
+  local win_id = vim.api.nvim_open_win(project_workbench_bufnr, true, {
+    relative = 'editor',
+    width = width,
+    height = height,
+    row = (ui.height - height) / 2,
+    col = (ui.width - width) / 2,
+    style = 'minimal'
+  })
 
   open_file_cmd = "e " .. project_workbench.filepath()
   vim.api.nvim_command(open_file_cmd)
 end
 
 function project_workbench.toggle()
-  if utils.directory_not_exist(vim.fn.expand('~/workspace')) then
-    utils.create_directory(vim.fn.expand('~/workspace'))
-  end
-
   -- override ui every time toggle is called
   ui = vim.api.nvim_list_uis()[1]
 
@@ -44,11 +46,11 @@ function project_workbench.toggle()
   if not project_workbench_initialized then
     project_workbench.initialize()
   elseif current_bufnr == project_workbench_bufnr then
-    utils.hide_workbench()
+    vim.api.nvim_command('hide')
   elseif buf_hidden == 0 and buf_info.windows[1] then
     vim.api.nvim_set_current_win(buf_info.windows[1])
   else
-    utils.show_workbench(project_workbench_bufnr)
+    vim.api.nvim_command('e ' .. project_workbench.filepath())
   end
 end
 
